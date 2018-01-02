b664a968869b39e7f92f18882c9e257f776b7331
Christopher Dorst <chris@dorst.email>
2018-01-02 00:39:36 -0600

    Add UserInfo

---
+++ DevOps.VersionControl.Structures.GitCommitUser/UserInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace DevOps.VersionControl.Structures.GitCommitUser
+{
+    public struct UserInfo
+    {
+        public UserInfo(string email, string name)
+        {
+            Email = email;
+            Name = name;
+        }
+
+        public string Email { get; set; }
+        public string Name { get; set; }
+    }
+}
